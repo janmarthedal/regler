01ad7e5d0f5fc73944090289f413bcb7ae148622
Jan Marthedal Rasmussen <jan@janmr.com>
2022-04-15 16:34:57 +0200

    Normalize WIP

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,13 @@ use crate::engine::Engine;
 use crate::parse::parse;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let (_, expr) = parse("1 + 2 + 3")?;
+    let (_, expr) = parse("1 + 2 + 3 * 4 * 5 + 6")?;
     println!("{:?}", expr);
     let mut engine = Engine::new();
     engine.init();
     let tree = engine.read_expr(&expr);
     Engine::print_tree(&tree);
+    let ntree = Engine::normalize(&tree);
+    Engine::print_tree(&ntree);
     Ok(())
 }
