705e3ceaab3fe3baf00e5d19d2a41c48345ff038
Jan Marthedal Rasmussen <jan@janmr.com>
2023-10-16 10:52:22 +0200

    Parse to tree type

--- test.hs
+++ test.hs
@@ -1,27 +1,34 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use <$>" #-}
+
 import Parsing ( Parser, (<|>), natural, symbol, parse )
 
-expr :: Parser Int
+data Tree = Sum Tree Tree | Prod Tree Tree | Integer Int
+            deriving Show
+
+expr :: Parser Tree
 expr = do t <- term
           do symbol "+"
              e <- expr
-             return (t + e)
+             return (Sum t e)
            <|> return t
 
-term :: Parser Int
+term :: Parser Tree
 term = do f <- factor
           do symbol "*"
              t <- term
-             return (f * t)
+             return (Prod f t)
            <|> return f
 
-factor :: Parser Int
+factor :: Parser Tree
 factor = do symbol "("
             e <- expr
             symbol ")"
             return e
-          <|> natural
+          <|> do n <- natural
+                 return (Integer n)
 
-eval :: String -> Int
+eval :: String -> Tree
 eval xs = case parse expr xs of
             Just (n, [])  -> n
             Just (_, out) -> error ("Unused input " ++ out)
