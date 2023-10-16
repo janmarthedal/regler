04aa43ee9d3adaf93dcec0c4c04f0fcfcddb6ee7
Jan Marthedal Rasmussen <jan@janmr.com>
2023-10-16 14:07:56 +0200

    Add operators Sub and Div

--- test.hs
+++ test.hs
@@ -3,7 +3,7 @@
 
 import Parsing ( Parser, (<|>), natural, symbol, parse )
 
-data Tree = Sum Tree Tree | Prod Tree Tree | Nat Int
+data Tree = Sum Tree Tree | Sub Tree Tree | Prod Tree Tree | Div Tree Tree | Nat Int
             deriving Show
 
 expr :: Parser Tree
@@ -14,7 +14,10 @@ expr' :: Tree -> Parser Tree
 expr' t1 = do symbol "+"
               t2 <- term
               expr' (Sum t1 t2)
-            <|> return t1
+            <|> do symbol "-"
+                   t2 <- term
+                   expr' (Sub t1 t2)
+                 <|> return t1
 
 term :: Parser Tree
 term = do f <- factor
@@ -24,7 +27,10 @@ term' :: Tree -> Parser Tree
 term' f1 = do symbol "*"
               f2 <- factor
               term' (Prod f1 f2)
-            <|> return f1
+            <|> do symbol "/"
+                   f2 <- factor
+                   term' (Div f1 f2)
+                 <|> return f1
 
 factor :: Parser Tree
 factor = do symbol "("
@@ -34,8 +40,8 @@ factor = do symbol "("
           <|> do n <- natural
                  return (Nat n)
 
-eval :: String -> Tree
-eval xs = case parse expr xs of
-            Just (n, [])  -> n
-            Just (_, out) -> error ("Unused input " ++ out)
-            Nothing       -> error "Invalid input"
+build :: String -> Tree
+build xs = case parse expr xs of
+             Just (n, [])  -> n
+             Just (_, out) -> error ("Unused input " ++ out)
+             Nothing       -> error "Invalid input"
